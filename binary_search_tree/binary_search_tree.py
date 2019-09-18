df990554ee99c0996215dc9804ad130c09b4a1c9
Arron M <marshall.arron@gmail.com>
2019-09-18 12:50:17 -0700

    Implement insert method

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -4,29 +4,47 @@ from dll_queue import Queue
 from dll_stack import Stack
 
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-    # Set our initial maximum value to initial value
-    self.max = value
+        # Set our initial maximum value to initial value
+        self.max = value
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        # if value > max, set max
+        if value > self.max:
+            self.max = value
 
-  def contains(self, target):
-    pass
+        # if value >= self.value
+        if value >= self.value:
+            # move down right
+            # if no node exists, this is our node
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+        else:
+            # value < self.value
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
 
-  def get_max(self):
-    return self.max
 
-  def for_each(self, cb):
-    # while self.left
-    #   recursive function with left node
-    # while self.right
-    #   recursive function with right node
-    pass
+    def contains(self, target):
+        pass
+
+    def get_max(self):
+        return self.max
+
+    def for_each(self, cb):
+        # while self.left
+        #   recursive function with left node
+        # while self.right
+        #   recursive function with right node
+        pass
 
 if __name__ == '__main__':
-  print('hello')
+    print('hello')
